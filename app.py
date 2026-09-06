87eb4b4832090954f1f9e66db149f8e8f984d566
shamimazad291737-collab <shamimazad291737@gmail.com>
2026-09-06 19:05:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -556,9 +556,9 @@ def handle_update(update):
             phone = data.replace("chk_otp_", "").replace("+", "").strip()
             order = get_order_by_phone(phone)
 
-            if order:
-            link = order[2]
-            try:
+                if order:
+                link = order[2]
+                try:
                 headers = {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
                 }

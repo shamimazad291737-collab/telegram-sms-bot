c2d196f5dd2f49a6e16c2ed90025eb94c74b3119
shamimazad291737-collab <shamimazad291737@gmail.com>
2026-09-07 00:10:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -643,10 +643,11 @@ class DummyServer(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b"Bot Engine Live and Healthy.")
-def run_web_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(('0.0.0.0', port), DummyServer)
-    server.serve_forever()
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
 def safe_execution_wrapper(upd):
     try:
